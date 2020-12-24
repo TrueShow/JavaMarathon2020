bf97e00a4ea17c231c1f7cdb003674a1dc759a53
TrueShow <regnad2004@gmail.com>
2020-12-24 10:39:00 +0300

    complete day1 tasks

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int loop = 10;
+        while(loop != 0) {
+            System.out.print("JAVA ");
+            loop--;
+        }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k;
+        k = 1 + (int) (Math.random() * 9);
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(i + " x " + k + " = " + i * k);
+        }
     }
 }
