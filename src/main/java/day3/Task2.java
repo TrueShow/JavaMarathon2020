5d8f7cdeef33e21389e02394a3512b06e71b281e
TrueShow <regnad2004@gmail.com>
2020-12-26 16:14:23 +0300

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while(scanner.hasNext()) {
+            double divider = scanner.nextDouble();
+            double divided = scanner.nextDouble();
+            if (divided == 0) {
+                break;
+            }
+            else {
+                System.out.println(divider / divided); ;
+            }
+        }
     }
 }
