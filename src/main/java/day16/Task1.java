29c84f1820e11b41b7215c17ecfa63d7f204927a
TrueShow <regnad2004@gmail.com>
2021-01-10 15:15:34 +0300

    complete day16 tasks

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,33 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File fileInput = new File("test.txt");
+        printResult(fileInput);
+    }
+
+    public static void printResult(File file) {
+        DecimalFormat df = new DecimalFormat();
+        int sum = 0;
+        double result = 0.0;
+        try (Scanner scanner = new Scanner(file)) {
+            String line = scanner.nextLine();
+            String[] stringNumbers = line.split(" ");
+
+            for (int i = 0; i < stringNumbers.length; i++) {
+                 sum += Integer.parseInt(stringNumbers[i]);
+            }
+            result = (double) sum / stringNumbers.length;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+        System.out.print(result + " --> " + df.format(result));
 
     }
 }
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,70 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        File fileOutput1000 = new File("file1.txt");
+        File fileOutput50 = new File("file2.txt");
 
+        try {
+            PrintWriter pw1000 = new PrintWriter(fileOutput1000);
+            PrintWriter pw50 = new PrintWriter(fileOutput50);
+
+            for (int i = 0; i < 1000; i++) {
+                pw1000.print((int)(Math.random() * 100) + " ");
+            }
+            pw1000.close();
+
+            Scanner scanner = new Scanner(fileOutput1000);
+            List<Integer> list = new ArrayList<>();
+            String[] numbers = scanner.nextLine().split(" ");
+            for (int i = 0; i < numbers.length; i++) {
+                list.add(Integer.parseInt(numbers[i]));
+            }
+
+            List<List<Integer>> lists = split(list, 20);
+            for (int i = 0; i < lists.size(); i++) {
+                int sum = 0;
+                for (int j = 0; j < lists.get(i).size(); j++) {
+                    sum += lists.get(i).get(j);
+                }
+                double result = (double) sum / lists.get(i).size();
+                pw50.print(result + " ");
+            }
+            pw50.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
+        printResult(fileOutput50);
+    }
+
+    public static void printResult(File file) {
+        double result = 0.0;
+        try {
+            Scanner scanner = new Scanner(file);
+            String[] numbers = scanner.nextLine().split(" ");
+
+            for (int i = 0; i < numbers.length; i++) {
+                result += Double.parseDouble(numbers[i]);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+        System.out.println((int)result);
+    }
+
+    public static List<List<Integer>> split(List<Integer> list, int splitter) {
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i < list.size(); i += splitter) {
+            lists.add(list.subList(i, i + splitter));
+        }
+        return lists;
     }
 }
